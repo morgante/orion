d2d8a53090d124eccf4b7470810bf200bf15c747
Morgante Pell <morgante.pell@morgante.net>
2013-12-29 04:18:03 -0500

    speeding is fun... make the ends always match

--- js/lifemap.js
+++ js/lifemap.js
@@ -35,56 +35,20 @@
         {
             tracks: ['red', 'green'],
             year: 40
+        },
+        {
+            tracks: ['green'],
+            year: 55
+        },
+        {
+            tracks: ['red'],
+            year: 45
         }
     ];
     var xOffset = 100;
     var yOffset = 100;
     var yearLength = 10;
-
-
-    var makeCar = function(path) {
-        var segments = path.segments;
-        var target = path.firstSegment;
-
-        var size = new paper.Size(10, 10);
-
-        var car = new paper.Path.Rectangle(target.point, size);
-
-        car.strokeColor = 'black';
-
-        var speed = 2;
-        var steps;
-
-        var dX;
-        var dY;
-
-        var goTowards = function(where) {
-            target = where;
-
-            steps = car.position.getDistance(target.point)/speed;
-
-            dX = (target.point.x - car.position.x)/steps;
-            dY = (target.point.y - car.position.y)/steps;
-
-        };
-
-        goTowards(target.next);
-
-        animators.push(function() {
-            if (car.position.getDistance(target.point) < 3) {
-                if (target.next) {
-                    goTowards(target.next);
-                } else {
-                    dX = 0;
-                    dY = 0;
-                }
-            }
-
-            // move it babe
-            car.position.x += dX;
-            car.position.y += dY;
-        });
-    };
+    var speed = 200;
 
     var onFrame = function(event) {
         animators.forEach(function(animator) {
@@ -124,6 +88,58 @@
         this.last = point;
     };
 
+    Track.prototype.addEnd = function(year) {
+        var x = yearLength * year;
+        var y = this.last.y;
+
+        this.path.add(new paper.Point(x, y));
+    };
+
+    Track.prototype.makeCar = function(speed) {
+        var path = this.path;
+
+        var segments = path.segments;
+        var target = path.firstSegment;
+
+        var size = new paper.Size(10, 10);
+
+        var car = new paper.Path.Rectangle(target.point, size);
+
+        car.strokeColor = path.strokeColor;
+
+        var steps;
+
+        var dX;
+        var dY;
+
+        var goTowards = function(where) {
+            target = where;
+
+            steps = car.position.getDistance(target.point)/speed;
+
+            dX = (target.point.x - car.position.x)/steps;
+            dY = (target.point.y - car.position.y)/steps;
+
+        };
+
+        goTowards(target.next);
+
+        animators.push(function() {
+            if (car.position.getDistance(target.point) < 3) {
+                if (target.next) {
+                    goTowards(target.next);
+                } else {
+                    dX = 0;
+                    dY = 0;
+                }
+            }
+
+            // move it babe
+            car.position.x += dX;
+            car.position.y += dY;
+        });
+    };
+
     var init = function() {
         // hide the lifemap
         $('.lifemap').hide();
@@ -133,6 +149,11 @@
         paper.setup(canvas);
 
         events.forEach(function(evt) {
+
+            // ensure properness
+            if (evt.duration === undefined) {
+                evt.duration = 2;
+            }
 
             // build proper event tracks
             var eventTracks = {};
@@ -151,20 +172,25 @@
 
             // determine the point for this event
             var y = _.reduce(_.map(evt.tracks, function(track) {
-                return track.last.y;
+                return track.start.y;
             }), function(sum, y) {
                 return sum + y;
             }, 0)/_.size(evt.tracks);
             var x = xOffset + (evt.year * yearLength);
             evt.point = new paper.Point(x, y);
 
-            console.log();
-
             _.each(evt.tracks, function(track) {
                 track.addEvent(evt);
             });
 
             evt.tracks = eventTracks;
+        });
+
+        _.each(tracks, function(track) {
+            // they all need to end at the same place
+            track.addEnd(75);
+
+            track.makeCar(track.path.length / speed);
         });
 
         paper.view.onFrame = onFrame;
